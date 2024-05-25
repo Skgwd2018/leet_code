ee5c6b9e27409a5c2e7a90e5f456193cc626d24a
Skgwd <skgwd2021@aliyun.com>
2024-05-25 20:39:05 +0800

    add 无重叠区间(区间集合问题)

--- src/main.rs
+++ src/main.rs
@@ -391,7 +391,10 @@ fn main() {
     let result = suggested_products(products, search_word);
     println!("suggested_products: {result:?}");
 
-    // println!("----- 无重叠区间 ------");
+    println!("----- 无重叠区间 ------");
+    let intervals = vec![vec![1, 2], vec![2, 3], vec![3, 4], vec![1, 3]];
+    let result = erase_overlap_intervals(intervals);
+    println!("erase_overlap_intervals: {result}");
 
     // println!("----- 每日温度 ------");
 
@@ -1430,5 +1433,42 @@ fn suggested_products(mut products: Vec<String>, search_word: String) -> Vec<Vec
     }
 
     result
+}
+//-----------------------------------------------------
+
+/// 无重叠区间(区间集合问题)
+// 给定一个区间的集合 intervals,其中 intervals[i] = [start, end] .返回 需要移除区间的最小数量，使剩余区间互不重叠
+// 输入: intervals = [[1,2],[2,3],[3,4],[1,3]]
+// 输出: 1
+// 解释: 移除 [1,3] 后，剩下的区间没有重叠。
+fn erase_overlap_intervals(mut intervals: Vec<Vec<i32>>) -> i32 {
+    // Vec::is_empty() 通常会比 Vec::len() == 0 执行效率稍微快
+    if intervals.len() < 2 { return 0; }
+
+    // intervals.sort_unstable();
+    // println!("sort_unstable ----> {intervals:?}"); // [[1, 2], [1, 3], [2, 3], [3, 4]]
+    intervals.sort_unstable_by_key(|v| v[1]);
+    // println!("sort_unstable_by_key ----> {intervals:?}"); // [[1, 2], [2, 3], [1, 3], [3, 4]]
+    // sort_unstable() 方法对 intervals 向量进行不稳定的就地排序。
+    // 它按照元素的自然顺序（对于 Vec<Vec<i32>> 类型，就是按照每个子向量的第一个元素，即 v[0]，进行比较）进行排序。
+    // 不稳定排序意味着相等的元素在排序后的相对顺序可能发生变化。
+    // intervals 调用 sort_unstable()，它会按照每个子向量的第一个元素进行排序。
+    // 如果 intervals 包含 [[1, 3], [2, 4], [1, 2]]，排序后的结果可能是 [[1, 3], [1, 2], [2, 4]]（注意 [1, 3] 和 [1, 2] 的顺序可能互换，因为排序是不稳定的）。
+    // sort_unstable_by_key() 方法允许你提供一个闭包（函数对象），它用于提取排序时要使用的键。
+    // 在这个例子中，闭包 |v| v[1] 表示每个子向量的第二个元素（即 v[1]）将用作排序的键。
+    // intervals 调用 sort_unstable_by_key(|v| v[1])，它会按照每个子向量的第二个元素进行排序。
+    // 如果 intervals 包含 [[1, 3], [2, 4], [1, 2]]，排序后的结果将是 [[1, 2], [1, 3], [2, 4]]，因为排序是基于每个子向量的第二个元素进行的。
+
+    let mut count = 0;
+    let mut end = intervals[0][1];
+    for i in 1..intervals.len() {
+        if intervals[i][0] >= end {
+            end = intervals[i][1];
+        } else {
+            count += 1;
+        }
+    }
+
+    count
 }
 //-----------------------------------------------------
