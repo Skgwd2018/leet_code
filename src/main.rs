3c99ca37d622ba64672e04fe2436cffa7fe6e583
Skgwd <skgwd2021@aliyun.com>
2024-08-01 12:30:48 +0800

    add 753.破解保险箱(DFS,图,欧拉回路)

--- src/main.rs
+++ src/main.rs
@@ -578,6 +578,10 @@ fn main() {
     let points = vec![[1, 1], [3, 2], [5, 3], [4, 1], [2, 3], [1, 4]];
     let answer = max_points(points);
     println!("max_points: {answer}");
+
+    println!("----- 753. 破解保险箱(DFS,图,欧拉回路) ------");
+    let answer = crack_safe(2, 2);
+    println!("crack_safe: {answer}");
 }
 
 /// 交替合并字符串
@@ -2657,5 +2661,46 @@ fn max_points(mut points: Vec<[i32; 2]>) -> i32 {
         if counter > points.len() as i32 / 2 { break; }
     }
     counter + 1
+}
+//-----------------------------------------------------
+
+// 有一个需要密码才能打开的保险箱。密码是 n 位数, 密码的每一位都是范围 [0, k - 1] 中的一个数字。
+// 保险箱有一种特殊的密码校验方法，可以随意输入密码序列，保险箱会自动记住 最后 n 位输入 ，如果匹配，则能够打开保险箱。
+// 例如，正确的密码是 "345" ，并且你输入的是 "012345" ：
+// 输入 0 之后，最后 3 位输入是 "0" ，不正确。
+// 输入 1 之后，最后 3 位输入是 "01" ，不正确。
+// 输入 2 之后，最后 3 位输入是 "012" ，不正确。
+// 输入 3 之后，最后 3 位输入是 "123" ，不正确。
+// 输入 4 之后，最后 3 位输入是 "234" ，不正确。
+// 输入 5 之后，最后 3 位输入是 "345" ，正确，打开保险箱。
+// 在只知道密码位数 n 和范围边界 k 的前提下，请你找出并返回确保在输入的 某个时刻 能够打开保险箱的任一 最短 密码序列。
+// 输入：n = 2, k = 2
+// 输出："01100"
+// 解释：对于每种可能的密码：
+// - "00" 从第 4 位开始输入。
+// - "01" 从第 1 位开始输入。
+// - "10" 从第 3 位开始输入。
+// - "11" 从第 2 位开始输入。
+// 因此 "01100" 可以确保打开保险箱。"01100"、"10011" 和 "11001" 也可以确保打开保险箱。
+fn crack_safe(n: i32, k: i32) -> String {
+    //Hierholzer 算法可以在一个欧拉图中找出欧拉回路。
+    fn dfs(node: i32, highest: i32, k: i32, seen: &mut HashSet<i32>, ans: &mut String) {
+        for x in 0..k {
+            let nei = node * 10 + x;
+            if !seen.contains(&nei) {
+                seen.insert(nei);
+                dfs(nei % highest, highest, k, seen, ans);
+                ans.push((x as u8 + b'0') as char);
+                // println!("{:?}",ans);
+            }
+        }
+    }
+
+    let mut seen = HashSet::new();
+    let mut ans = String::new();
+    let highest = 10_i32.pow(n as u32 - 1);
+    dfs(0, highest, k, &mut seen, &mut ans);
+    ans.push_str(&"0".repeat((n - 1) as usize));
+    ans
 }
 //-----------------------------------------------------
