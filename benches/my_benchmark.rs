25514d351d82a1a12083fd8fd0b8c1155918476e
skgwd <skgwd2021@aliyun.com>
2024-12-31 12:00:24 +0800

    update

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -7,7 +7,7 @@ fn fibonacci(n: u64) -> u64 {
         n => fibonacci(n - 1) + fibonacci(n - 2),
     }
 }
-// time:   [39.721 µs 39.843 µs 39.969 µs]
+// time:   [17.235 µs 17.297 µs 17.361 µs]
 
 // 被测试的函数(优化后)
 fn fibonacci2(n: u64) -> u64 {
@@ -26,7 +26,7 @@ fn fibonacci2(n: u64) -> u64 {
         }
     }
 }
-// time:   [21.141 ns 21.235 ns 21.342 ns]
+// time:   [4.9158 ns 4.9403 ns 4.9663 ns]
 
 pub fn fibonacci3(n: usize) -> usize {
     if n < 2 { return n; }
@@ -38,15 +38,15 @@ pub fn fibonacci3(n: usize) -> usize {
     //     dp[i] = dp[i - 1] + dp[i - 2];
     // }
     // dp[n]
-    // time:   [203.23 ns 204.10 ns 205.26 ns]
+    // time:   [71.225 ns 71.475 ns 71.772 ns]
 
     (2..=n).fold((0, 1), |(dp1, dp2), _| (dp2, dp1 + dp2)).1
 }
-// time:   [18.958 ns 18.998 ns 19.046 ns]
+// time:   [6.4998 ns 6.6538 ns 6.8004 ns]
 
 // 基准测试函数
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| b.iter(|| fibonacci3(black_box(20))));
+    c.bench_function("fib 20", |b| b.iter(|| fibonacci2(black_box(20))));
 }
 
 criterion_group!(benches, criterion_benchmark);
@@ -57,3 +57,4 @@ criterion_main!(benches);
 
 // 执行指定基准测试
 // cargo bench --bench my_benchmark
+// 使用参考: https://bheisler.github.io/criterion.rs/book/getting_started.html
